38874969ebb5561589886b2a381bead07a29e3f2
liangzuobin <liangzuobin@meican.com>
2017-01-12 18:52:23 +0800

    prompt extras

--- init.el
+++ init.el
@@ -594,3 +594,10 @@
 
 (require 'bash-completion)
 (bash-completion-setup)
+
+(with-eval-after-load "esh-opt"
+  (require 'virtualenvwrapper)
+  (venv-initialize-eshell)
+  (autoload 'epe-theme-lambda "eshell-prompt-extras")
+  (setq eshell-highlight-prompt nil
+        eshell-prompt-function 'epe-theme-lambda))
